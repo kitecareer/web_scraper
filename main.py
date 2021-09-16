406eb5f5cfbdfb56066b321c963c21e776062b85
kirubakaranCR7 <wowsoccer7@gmail.com>
2021-09-16 15:09:05 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+# For using sleep function because selenium
+# works only when the all the elemets of the
+# page is loaded.
+import time
+
+from PIL import Image
+browser = webdriver.Chrome("F:\chromedriver_win32 (2)\chromedriver.exe")
+
+url = "https://www.instagram.com"  # You can paste any id
+
+browser.get(url)
+time.sleep(3)
+
+time.sleep(5)
+username=browser.find_element_by_css_selector("input[name='username']")
+password=browser.find_element_by_css_selector("input[name='password']")
+username.clear()
+password.clear()
+username.send_keys("muthu_krishnan74")
+password.send_keys("muthukrishnan")
+login = browser.find_element_by_css_selector("button[type='submit']").click()
+time.sleep(5)
+browser.save_screenshot("screenshot.png")
+
+# Loading the image
+image = Image.open("screenshot.png")
+
+# Showing the image
+image.show()
